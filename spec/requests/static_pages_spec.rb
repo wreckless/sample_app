d794500a89844dad336158524c83c3fa5544263f
Horlbeck <ehorlbeck@gmail.com>
2013-03-27 12:26:18 -0400

    added Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,11 @@ describe "Static pages" do
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+
+  describe "Contact page" do
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
 end
